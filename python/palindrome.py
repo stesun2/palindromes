569ca58640092a27371e501d8c1b55f0b72c7757
stesun2 <ss27@illinois.edu>
2021-05-30 17:26:12 -0500

    work in progress

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,22 @@
 def palindrome(word):
     # Write code here
+
+    word = word.lower()
+    pali_word = word[::-1]
+
+    # print('Original:', word)
+    # print('Palidrome word: ', pali_word)
+
+    if (word == pali_word):
+        return True
+    else:
+        return False
+
+
+# print(palindrome('racecar'))
+# print(palindrome('Noon'))
+# print(palindrome('ciVic'))
+# print(palindrome('nice'))
+# print(palindrome('bomb'))
+# print(palindrome(434))
+# print(palindrome(123))
